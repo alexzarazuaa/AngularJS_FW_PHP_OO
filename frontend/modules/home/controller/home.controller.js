0fc8fd20aaaaeeac6f31893fafc125504ffbf95b
Alex Zarazua <alexzarazua88@gmail.com>
2020-05-23 13:09:36 +0200

    Home  migrated finish

--- frontend/modules/home/controller/home.controller.js
+++ frontend/modules/home/controller/home.controller.js
@@ -1,22 +1,24 @@
 mastersport.controller('homeCtrl', function ($scope, data_carousel, data_categories, count_categoria, services, $uibModal, CommonService) {
-  console.log("controller home");
-  console.log(data_carousel);
+  // console.log("controller home");
+  //console.log(data_carousel);
   //  console.log(data_categories);
+
   cont = 2
   $scope.myInterval = 5000;
   $scope.noWrapSlides = false;
   $scope.active = 0;
-  $scope.slides=data_carousel;
-   console.log(data_categories.slice(0, cont));
+  $scope.slides = data_carousel;
+  $scope.prods = data_categories.slice(0, cont)
   $scope.index = 0;
-  //   $scope.breeds = bbreeds.slice(0,cont);
+  //console.log($scope.prods);
 
-  //   $scope.showMore = function(){
-  //     cont=cont+2;
-  //     $scope.breeds = bbreeds.slice(0,cont);
-  //     if (cont == 6) {
-  //       var prov = document.querySelector('#click_scroll');
-  //       prov.remove();
-  //     }
-  // }
+  $scope.loadmore = function () {
+    cont = cont + 2;
+    $scope.prods = data_categories.slice(0, cont)
+    if (cont == 8) {
+      var prov = document.querySelector('#click_scroll');
+      prov.remove();
+    }
+  }//end_loadmore 
+
 })//END_HOME_CONTROLLER
